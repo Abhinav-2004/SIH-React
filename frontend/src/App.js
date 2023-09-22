fc548ef92f1f4d2f5f676bf38973109083065118
addymistrel <addymistrel@gmail.com>
2023-09-22 07:53:19 +0530

    api tested succesfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [tmp, setTmp] = useState("Hello");
+  async function getVal() {
+    const data = await axios
+      .get("http://127.0.0.1:8000/api")
+      .then((res) => {
+        console.log(res.data);
+        setTmp(res.data);
+      })
+      .catch((err) => {
+        console.log("aditya");
+        console.log(err);
+      });
+  }
+  useEffect(() => {
+    getVal();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-[red]'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{tmp}</h1>
     </div>
   );
 }
