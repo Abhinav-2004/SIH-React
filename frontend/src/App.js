e58b76216eae09c127f391536faa95402f7521d8
Abhinav Singh <kittusinghranchi@gmail.com>
2023-09-22 19:07:03 +0530

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,17 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SingleInput from "./pages/singleInput";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
+  return (<>
+    <Navbar/>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SingleInput />} />
       </Routes>
     </BrowserRouter>
-  );
+  </>);
 }
 
 export default App;
